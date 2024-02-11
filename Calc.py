cf127f93bd342c7a0583e87abbd7c12360d1467d
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-02-11 15:25:36 -0300

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,22 @@
+numC = int(input("1: Somar 2: Subtrair 3: Divisão 4: Multiplicaçao "))
+if numC > 4:
+    print("Coloque uma opção valida.")
+    quit()
+
+num1 = int(input("Primeiro Numero: "))
+num2 = int(input("Segundo Numero: "))
+if numC == 1:
+    num3 = num1 + num2
+    print("A Resposta de Sua Soma é:",num3)
+if numC == 2:
+    num4 = num1 - num2
+    print("A Resposta de Sua Subtração é:", num4)
+if numC == 3:
+    num5 = num1 / num2
+    print("A Resposta de Sua Divisão é:", num5)
+if numC == 4:
+    num6 = num1 * num2
+    print("A Resposta de Sua Multiplicação é:",num6)
+
+
+
