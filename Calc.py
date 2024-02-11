38b99cc6bf5238ef319b1f2bf4063bdbbe3a2c26
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-02-11 16:31:20 -0300

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,21 +1,29 @@
-numC = int(input("1: Somar 2: Subtrair 3: Divisão 4: Multiplicaçao "))
+// Pergunta ao usuario a opção que deseja e armazena na variavel numC
+numC = int(input("1: Somar 2: Subtrair 3: Multiplicação 4: Divisão "))
+// Verifica se numC é maior que 4 assim a opção sendo invalida
 if numC > 4:
     print("Coloque uma opção valida.")
     quit()
-
+// Variavel num1 onde tem o primeiro numero
 num1 = int(input("Primeiro Numero: "))
+// Variavel num2 onde tem o segundo numero
 num2 = int(input("Segundo Numero: "))
+// Verifica se numC é 1 assim somando os valores
 if numC == 1:
-    num3 = num1 + num2
-    print("A Resposta de Sua Soma é:",num3)
+    Resultado = num1 + num2
+    print("A Resposta de Sua Soma é:", Resultado)
+// Verifica se numC é 2 assim subtraindo os valores
 if numC == 2:
-    num4 = num1 - num2
-    print("A Resposta de Sua Subtração é:", num4)
+    Resultado = num1 - num2
+    print("A Resposta de Sua Subtração é:", Resultado)
+// Verifica se numC é 3 assim multiplicando os valores
 if numC == 3:
-    num5 = num1 / num2
-    print("A Resposta de Sua Divisão é:", num5)
+    Resultado = num1 / num2
+    print("A Resposta de Sua Multiplicação é:", Resultado)
+// Verifica se numC é 4 assim dividindo os valores
 if numC == 4:
-    num6 = num1 * num2
-    print("A Resposta de Sua Multiplicação é:",num6)
-
+    Resultado = num1 * num2
+    print("A Resposta de Sua Divisão é:", Resultado)
+    
+// Fiz para não fechar o cmd ao abrir
 input()
