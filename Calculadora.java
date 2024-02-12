2c5e92766eb21ee3f74f3f999825fad669e8ed22
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-02-12 09:21:48 -0300

    Update and rename Calc.java to Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -6,9 +6,14 @@ public class Calculadora {
     // Cria um objeto Scanner para ler a entrada do usuário
     Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Escolha a opção: ");
+    System.out.println("1: Soma");
+    System.out.println("2: Subtração");
+    System.out.println("3: Multiplicação");
+    System.out.println("4: Divisão");
     // Pergunta ao usuario o tipo de conta que ira fazer armazenando resultado na variavel numC
-    System.out.print("1: Soma 2: Substituição 3: Multiplicação 4: Divisão ");
-    int numC = scanner.nextInt();
+    System.out.print("");
+    double numC = scanner.nextDouble();
     // Verifica se a opção escolhida for maior que 4 sendo assim invalida
     if (numC > 4) {
         System.out.print("Digite uma opção valida");
@@ -16,12 +21,12 @@ public class Calculadora {
     }
     // Pergunta o primeiro numero ao usuario armazenando na variavel num1
     System.out.print("Primeiro Numero: ");
-    double num1 = scanner.nextInt();
+    double num1 = scanner.nextDouble();
     // Pergunta o segundo numero ao usuario armazenando na variavel num2
     System.out.print("Segundo Numero: ");
-    double num2 = scanner.nextInt();
+    double num2 = scanner.nextDouble();
     // Variavel onde se armazena o resultado
-    double Resultado;
+    double Resultado = 0;
 
     // Verifica se a opção escolhida foi 1 assim somando o resultado
     if (numC == 1) {
